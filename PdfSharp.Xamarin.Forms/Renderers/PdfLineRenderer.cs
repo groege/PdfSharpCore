af25a73be5e1ffc09a9ab04eadeac5ce5bf77fd5
akgulebubekir <ebubekirakgul91@gmail.com>
2020-12-28 15:03:19 +0100

    Renderers implemented for Ellipse, Line and Rectangle Shapes

---
+++ PdfSharp.Xamarin.Forms/Renderers/PdfLineRenderer.cs
@@ -0,0 +1,25 @@
+﻿using PdfSharp.Xamarin.Forms.Attributes;
+using PdfSharp.Xamarin.Forms.Extensions;
+using PdfSharpCore.Drawing;
+using Xamarin.Forms.Shapes;
+
+namespace PdfSharp.Xamarin.Forms.Renderers
+{
+	[PdfRenderer(ViewType = typeof(Line))]
+	public class PdfLineRenderer : PdfRendererBase<Line>
+	{
+		public override void CreatePDFLayout(XGraphics page, Line line, XRect bounds, double scaleFactor)
+		{
+			if (line.BackgroundColor != default)
+				page.DrawRectangle(line.BackgroundColor.ToXBrush(), bounds);
+
+			var x1 = bounds.X + line.X1 * bounds.Width / line.Width;
+			var x2 = bounds.X + line.X2 * bounds.Width / line.Width;
+			var y1 = bounds.Y + line.Y1 * bounds.Height / line.Height;
+			var y2 = bounds.Y + line.Y2 * bounds.Height / line.Height;
+
+			if (line.Stroke != null)
+				page.DrawLine(line.Stroke.ToXPen(line.StrokeThickness * scaleFactor), x1, y1, x2, y2);
+		}
+	}
+}
